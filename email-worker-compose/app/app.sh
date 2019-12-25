41c23e100fa84d7ee236c1727a7300606c3ffa39
Tiago <sudeval@gmail.com>
2019-12-25 16:38:53 -0300

    adjustment with psycopg2 version

--- email-worker-compose/app/app.sh
+++ email-worker-compose/app/app.sh
@@ -2,5 +2,6 @@
 # instalando o framework bottle
 # pip install bottle==0.12.13
 # adicionando a dependencia do banco (psycopg2)
-pip install bottle==0.12.13 psycopg2==2.7.1
+# o curso recomenda a versao 2.7.1, mas tive problema com a instalacao dela.
+pip install bottle==0.12.13 psycopg2==2.7.4
 python -u sender.py
