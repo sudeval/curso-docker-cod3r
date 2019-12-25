9bf11eccc863f3264d78a3cc7f9ecc39061ac219
Tiago <sudeval@gmail.com>
2019-12-25 15:32:09 -0300

    adding networks and persistence in database

--- email-worker-compose/app/app.sh
+++ email-worker-compose/app/app.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # instalando o framework bottle
-pip install bottle==0.12.13
+# pip install bottle==0.12.13
+# adicionando a dependencia do banco (psycopg2)
+pip install bottle==0.12.13 psycopg2==2.7.1
 python -u sender.py
--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,10 +1,26 @@
+import psycopg2
 from bottle import route, run, request
+
+# data source naming / db (nome do serviço) no host é para evitar definir um IP.
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+def register_message(assunto, mensagem):
+    conn = psycopg2.connect(DSN)
+    cursor = conn.cursor()
+    cursor.execute(SQL, (assunto, mensagem))
+    conn.commit()
+    cursor.close()
+
+    print('Mensagem registrada  !')
 
 @route('/', method='POST')
 def send():
     # assunto e mensagens são os campos (name) existente no formulario
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
+
+    register_message(assunto, mensagem)
     return 'Mensagens enfileirada ! Assunto: {} Mensagem: {}'.format(
         assunto, mensagem
     )
