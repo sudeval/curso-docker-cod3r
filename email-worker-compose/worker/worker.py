ede78cf9acedc4375e1d77ada6d43c9d94c0e1fa
Tiago <sudeval@gmail.com>
2019-12-26 00:40:12 -0300

    add waiting message and correcting sintax error

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -5,11 +5,12 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+    print('Aguardando mensagens ...')
     # laço infinito para consumir as mensagens
-    while: True:
+    while True:
         # pegando a mensagem dentro da fila 'sender'
         mensagem = json.loads(r.blpop('sender')[1])
         # simulando envio do email
         print('Mandando a mensagem:', mensagem['assunto'])
-        sleep(randit(15, 45))
+        sleep(randint(15, 45))
         print('Mensagem:', mensagem['assunto'], 'enviada')
